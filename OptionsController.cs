0b42305f18472b4867aefe4efd18ee46d740b908
Belevar <tlustywalec@gmail.com>
2016-01-11 23:48:17 +0100

    Added options controller.

---
+++ OptionsController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsController : MonoBehaviour
+{
+	public Slider soundsSlider;
+	public Slider musicSlider;
+	public LevelManager levelManager;
+	private MusicPlayer musicPlayer;
+	// Use this for initialization
+	void Start ()
+	{
+		musicPlayer = GameObject.FindObjectOfType<MusicPlayer> ();
+		musicSlider.value = PlayerPrefsManager.getMusicVolume ();
+		soundsSlider.value = PlayerPrefsManager.getSoundsVolume ();
+	}
+
+	void Update ()
+	{
+		musicPlayer.changeVolume (musicSlider.value);
+	}
+
+	public void saveAndExit ()
+	{
+		PlayerPrefsManager.setMusicVolume (musicSlider.value);
+		PlayerPrefsManager.setSoundsVolume (soundsSlider.value);
+		levelManager.loadScene ("start");
+	}
+}
